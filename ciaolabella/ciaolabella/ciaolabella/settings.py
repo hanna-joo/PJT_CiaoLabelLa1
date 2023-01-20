8fcb47fb76d13c8fea7e424ed4474ebd4b388248
Hanna Joo <69278218+hanna-joo@users.noreply.github.com>
2023-01-20 10:02:43 +0900

    Update settings.py

--- ciaolabella/ciaolabella/ciaolabella/settings.py
+++ ciaolabella/ciaolabella/ciaolabella/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_#t9zl4dmw@+3n)ut4xsikn5qbo@g4s1c!enq@l$2=3duq1(u-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
